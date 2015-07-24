df247ab0e661810b905bb58329d2753386cb8168
rensmms <olivier.or.renard@gmail.com>
2015-07-24 15:51:13 +0200

    Espace

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css.sass, and all non-JS/CSS in app/initializers folder are already added.
 # Rails.application.config.initializers.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( ckeditor/* )
+
